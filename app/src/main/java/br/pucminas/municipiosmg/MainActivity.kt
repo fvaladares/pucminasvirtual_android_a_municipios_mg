45ee50398e122e4047617d19f5d03720c60df138
Fabricio Valadares <fabricio.valadares@gmail.com>
2022-07-29 17:14:00 -0300

    Adicionando licença

--- app/src/main/java/br/pucminas/municipiosmg/MainActivity.kt
+++ app/src/main/java/br/pucminas/municipiosmg/MainActivity.kt
@@ -1,5 +1,22 @@
 package br.pucminas.municipiosmg
-
+/*
+ * Copyright (C) 2022 Professor Fabrício Valadares -- Puc Minas Virtual
+ * Code based on a example created by Google Developer Trainning
+ *
+ *    https://github.com/google-developer-training/android-basics-kotlin-words-app/tree/activities
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
